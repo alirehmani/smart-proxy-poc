77f14c4f5d779e7ac6de911a47d1be3d6676fd2c
alirehmani <osman.highbrow@gmail.com>
2023-09-26 00:50:27 +0500

    zen row implemented

--- Program.cs
+++ Program.cs
@@ -1,47 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-
-class Program
+﻿using System.Net;
+using RestSharp;
+namespace TestApplication
 {
-    static void Main(string[] args)
+    class Test
     {
-        Task t = new Task(DownloadPageAsync);
-        t.Start();
-        Console.ReadLine();
-    }
-
-    static async void DownloadPageAsync()
-    {
-        string page = "http://ip.smartproxy.com/";
-
-        var proxy = new WebProxy("gate.smartproxy.com:1000")
+        static void Main(string[] args)
         {
-            UseDefaultCredentials = false,
+            var options = new RestClientOptions()
+            {
+                Proxy = new WebProxy()
+                {
+                    Address = new Uri("http://proxy.zenrows.com:8001"),
+                    Credentials = new NetworkCredential("4fe05cea01dae79bfb53fe87793cafcab7c804b5", "")
+                },
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
 
-            Credentials = new NetworkCredential(
-                userName: "spbo7f3xbs",
-                password: "iq4MkjmlkC1z3vT1Rn")
-        };
+            var client = new RestClient(options);
+            var request = new RestRequest("https://theparadise.pk");
 
-        var httpClientHandler = new HttpClientHandler()
-        {
-            Proxy = proxy,
-        };
-
-        var client = new HttpClient(handler: httpClientHandler, disposeHandler: true);
-        var response = await client.GetAsync(page);
-        using (HttpContent content = response.Content)
-        {
-            string result = await content.ReadAsStringAsync();
-            Console.WriteLine(result);
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-
+            var response = client.Get(request);
+            Console.WriteLine(response.Content);
         }
     }
 }
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Task t = new Task(DownloadPageAsync);
+//        t.Start();
+//        Console.ReadLine();
+//    }
+
+
+
+//static async void DownloadPageAsync()
+//{
+//    string page = "http://ip.smartproxy.com/";
+
+//    var proxy = new WebProxy("gate.smartproxy.com:1000")
+//    {
+//        UseDefaultCredentials = false,
+
+//        Credentials = new NetworkCredential(
+//            userName: "spbo7f3xbs",
+//            password: "iq4MkjmlkC1z3vT1Rn")
+//    };
+
+//    var httpClientHandler = new HttpClientHandler()
+//    {
+//        Proxy = proxy,
+//    };
+
+//    var client = new HttpClient(handler: httpClientHandler, disposeHandler: true);
+//    var response = await client.GetAsync(page);
+//    using (HttpContent content = response.Content)
+//    {
+//        string result = await content.ReadAsStringAsync();
+//        Console.WriteLine(result);
+//        Console.WriteLine("Press any key to exit.");
+//        Console.ReadKey();
+
+//    }
+//}
+//}
